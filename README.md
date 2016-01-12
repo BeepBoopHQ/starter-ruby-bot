845adebe3b1ebcad658a84f10b1c9032971443f6
chris-skud <chris.skud@gmail.com>
2016-01-12 16:52:34 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# starter-ruby-bot
+
+## Overview
+Please visit the [Beep Boop Overview](https://beepboophq.com/docs/article/overview) to get the scoop on the the Beep Boop hosting platform and the role of the bot.yml and Dockerfile files.
+
+Slack API documentation is [here](https://api.slack.com/)
+
+## Usage:
+
+...
+
---
+++ bot.rb
@@ -0,0 +1,85 @@
+require 'slack-ruby-client'
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_TOKEN']
+  fail 'Missing ENV[SLACK_TOKEN]!' unless config.token
+end
+
+client = Slack::RealTime::Client.new
+
+# listen for hello (connection) event - https://api.slack.com/events/hello
+client.on :hello do
+  puts "Connected '#{client.self['name']}' to '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com."
+end
+
+# listen for channel_joined event - https://api.slack.com/events/channel_joined
+client.on :channel_joined do |data|
+  if joiner_is_bot?(data)
+    client.message channel: data['channel']['id'], text: starter_text
+  else
+    puts "Someone far less important than #{client.self['name']} joined #{data['channel']['id']}"
+  end
+end
+
+# listen for message event - https://api.slack.com/events/message
+client.on :message do |data|
+
+  case data['text']
+  when 'hi', 'bot hi' then
+    client.typing channel: data['channel']
+    client.message channel: data['channel'], text: "Hello <@#{data['user']}>."
+
+    if direct_message?(data)
+      client.message channel: data['channel'], text: "I feel like I've known you forever."
+    end
+
+  when 'attachment', 'bot attachment' then
+    # attachment messages require using web_client
+    client.web_client.chat_postMessage(post_message_payload(data))
+
+  when bot_mentioned(client)
+    client.message channel: data['channel'], text: 'You really do care about me. :heart:'
+  end
+end
+
+def direct_message?(data)
+  # direct message channles start with a 'D'
+  data['channel'][0] == 'D'
+end
+
+def bot_mentioned(client)
+  # match on any instances of `<@bot_id>` in the message
+  /\<\@#{client.self['id']}\>+/
+end
+
+def joiner_is_bot?(data)
+ /^\<\@#{client.self['id']}\>/.match data['channel']['latest']['text']
+end
+
+def starter_text
+  %Q("Hello, I'm the BeepBoop starter bot. I don't do much yet, but I would like it if you talked to me anyway: \n
+      `bot hi`\n
+      `bot attachment`\n
+      `@<your bot\'s name>`\n)
+end
+
+def post_message_payload(data)
+  main_msg = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+  {
+    channel: data['channel'],
+      as_user: true,
+      attachments: [
+        {
+          fallback: main_msg,
+          pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
+          title: 'Host, deploy and share your bot in seconds.',
+          image_url: 'https://beepboophq.com/bot-1.22f6fb.png',
+          title_link: 'https://beepboophq.com/',
+          text: main_msg,
+          color: '#7CD197'
+        }
+      ]
+  }
+end
+
+client.start!
